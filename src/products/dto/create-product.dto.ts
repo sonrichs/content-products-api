11279323f4a1a88df681fb3a738b901c58fab72b
Rodrigo Cepeda <rodrigo.cepeda.1993@gmail.com>
2025-11-08 11:28:32 -0300

    Add validation rules to CreateProductDto and UpdateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProductDto {}
+import { IsNumber, IsString, Length, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @Length(1, 100)
+  sku: string;
+
+  @IsString()
+  @Length(1, 255)
+  name: string;
+
+  @IsString()
+  @Length(1, 255)
+  brand: string;
+
+  @IsString()
+  @Length(1, 255)
+  model: string;
+
+  @IsString()
+  @Length(1, 255)
+  category: string;
+
+  @IsString()
+  @Length(1, 255)
+  color: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsString()
+  @Length(3, 3)
+  currency: string;
+
+  @IsNumber()
+  @Min(0)
+  stock: number;
+}
