1307ee0b52f7a18626e2dcfa66d3be6a7e9b7371
Rodrigo Cepeda <rodrigo.cepeda.1993@gmail.com>
2025-11-08 11:41:37 -0300

    Refactor API endpoints and set up versioning for ProductsController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,7 +11,7 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-@Controller('products')
+@Controller({ path: 'products', version: '1' })
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
