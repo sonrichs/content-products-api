000dc26568640e21e02307177909c909391c322c
Rodrigo Cepeda <rodrigo.cepeda.1993@gmail.com>
2025-11-07 23:49:42 -0300

    Implement Product entity with attributes

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,40 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar', { unique: true, length: 100 })
+  sku: string;
+
+  @Column('varchar', { length: 255 })
+  name: string;
+
+  @Column('varchar', { length: 255 })
+  brand: string;
+
+  @Column('varchar', { length: 255 })
+  model: string;
+
+  @Column('varchar', { length: 255 })
+  category: string;
+
+  @Column('varchar', { length: 255 })
+  color: string;
+
+  @Column('numeric', { precision: 10, scale: 2 })
+  price: number;
+
+  @Column('char', { length: 3 })
+  currency: string;
+
+  @Column('numeric', { precision: 10, scale: 0 })
+  stock: number;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
